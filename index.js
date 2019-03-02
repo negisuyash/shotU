4288bd74d2efdf01faf50a423454406e498fb26d
Suyash Negi <negisuyash1998@gmail.com>
2019-03-02 19:04:43 +0530

    chaneg1

--- index.js
+++ index.js
@@ -8,7 +8,8 @@
 console.log("in index.js")
 
 
-
+var base64=require('base-64')
+var utf8=require('utf8')
 
 var getUserMedia = require('getusermedia')
 
@@ -23,21 +24,28 @@ getUserMedia({ video: true, audio: true }, function (err, stream) {
   })
 
   peer.on('signal', function (data) {
-    document.getElementById('yourid').value = JSON.stringify(data)
+    document.getElementById('yourid').value = base64.encode(utf8.encode(JSON.stringify(data)))
   })
 
   document.getElementById('connect').addEventListener('click', function () {
-    var otherId = JSON.parse(document.getElementById('otherid').value)
+    var otherId = JSON.parse(utf8.decode(base64.decode(document.getElementById('otherid').value)))
     peer.signal(otherId)
   })
 
+
   document.getElementById('send').addEventListener('click', function () {
-    var yourMessage = document.getElementById('yourMessage').value
-    peer.send(yourMessage)
+    if (document.getElementById('yourMessage').value.trim() != "")
+    {
+      var yourMessage = document.getElementById('yourMessage').value
+      document.getElementById('yourMessage').value=" "
+      document.getElementById('messages').textContent+=' YOU : '+ yourMessage +'\n'
+      peer.send(yourMessage)
+    }
+ 
   })
 
   peer.on('data', function (data) {
-    document.getElementById('messages').textContent += data + '\n'
+    document.getElementById('messages').textContent +=' OTHER : '+ data + '\n'
   })
 
   peer.on('stream', function (stream) {
